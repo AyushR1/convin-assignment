af39358abc6d5b3dc18156fc54e3c10d94bd6aa2
AyushR1 <ayushrathore12501@gmail.com>
2023-03-01 23:37:36 +0530

    Implement the calender api
    Signed-off-by: AyushR1 <ayushrathore12501@gmail.com>

--- oauth20/urls.py
+++ oauth20/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('calendar/init/', views.GoogleCalendarInitView, name='google_permission'),
+    path('calendar/redirect/', views.GoogleCalendarRedirectView, name='google_redirect'),
     path('', views.index, name='index'),
 ]
--- oauth20/views.py
+++ oauth20/views.py
@@ -5,6 +5,11 @@ from django.shortcuts import redirect
 
 import google.oauth2.credentials
 import google_auth_oauthlib.flow
+import googleapiclient.discovery
+
+# solves insecure transport error
+import os
+os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
 
 CREDENTIALS_SECRET = "credentials.json"
 SCOPES = ['https://www.googleapis.com/auth/calendar']
@@ -28,6 +33,54 @@ def GoogleCalendarInitView(request):
         include_granted_scopes='true')
 
     request.session['state'] = state
-    response= redirect(authorization_url)
+    response = redirect(authorization_url)
 
     return response
+
+
+@api_view(['GET'])
+def GoogleCalendarRedirectView(request):
+    state = request.session['state']
+
+    flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
+        CREDENTIALS_SECRET, scopes=SCOPES, state=state)
+    flow.redirect_uri = REDIRECT
+
+    authorization_response = request.get_full_path()
+    flow.fetch_token(authorization_response=authorization_response)
+
+    credentials = flow.credentials
+    request.session['credentials'] = credentials_to_dict(credentials)
+
+    if 'credentials' not in request.session:
+        return redirect('v1/calendar/init')
+
+    credentials = google.oauth2.credentials.Credentials(
+        **request.session['credentials'])
+
+    service = googleapiclient.discovery.build(
+        'calendar', 'v3', credentials=credentials)
+
+    calendar_list = service.calendarList().list().execute()
+
+    calendar_id = calendar_list['items'][0]['id']
+
+    events = service.events().list(calendarId=calendar_id).execute()
+
+    events_list_append = []
+    if not events['items']:
+        print('No data found.')
+        return Response({"message": "No data found or user credentials invalid."})
+    else:
+        for events_list in events['items']:
+            events_list_append.append(events_list)
+        return Response({"events": events_list_append})
+
+
+def credentials_to_dict(credentials):
+    return {'token': credentials.token,
+            'refresh_token': credentials.refresh_token,
+            'token_uri': credentials.token_uri,
+            'client_id': credentials.client_id,
+            'client_secret': credentials.client_secret,
+            'scopes': credentials.scopes}
