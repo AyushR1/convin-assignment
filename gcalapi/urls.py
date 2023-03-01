c84d50197fbbe8ec2e574309e99a68ad072d83c0
AyushR1 <ayushrathore12501@gmail.com>
2023-03-01 23:04:46 +0530

    Implement OAuth
    Signed-off-by: AyushR1 <ayushrathore12501@gmail.com>

--- gcalapi/urls.py
+++ gcalapi/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from oauth20 import views
 
 urlpatterns = [
+    path('',views.index),
     path('rest/v1/', include('oauth20.urls')),
     path('admin/', admin.site.urls),
 ]
--- oauth20/views.py
+++ oauth20/views.py
@@ -1,5 +1,33 @@
 from django.http import HttpResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from django.shortcuts import redirect
+
+import google.oauth2.credentials
+import google_auth_oauthlib.flow
+
+CREDENTIALS_SECRET = "credentials.json"
+SCOPES = ['https://www.googleapis.com/auth/calendar']
+REDIRECT = 'http://127.0.0.1:8000/rest/v1/calendar/redirect'
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    button_html = '<a href="http://127.0.0.1:8000/rest/v1/calendar/init/">Login with Google</a>'
+    return HttpResponse(button_html)
+
+
+@api_view(['GET'])
+def GoogleCalendarInitView(request):
+    flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
+        CREDENTIALS_SECRET, scopes=SCOPES)
+
+    flow.redirect_uri = REDIRECT
+
+    authorization_url, state = flow.authorization_url(
+        access_type='offline',
+        include_granted_scopes='true')
+
+    request.session['state'] = state
+    response= redirect(authorization_url)
+
+    return response
